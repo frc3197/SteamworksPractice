d0f73327018d492a35726c20962514a1110343d1
3197Software <3197Software@gmail.com>
2018-05-08 21:20:04 -0500

    more

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -3,43 +3,50 @@
 #include "WPILib.h"
 #include "ctre/Phoenix.h"
 
-
 class Robot: public frc::IterativeRobot {
 public:
-	void RobotInit() {
-
+	void RobotInit() override {
+		climberFollow.Follow(climber);
+		shoot.SetInverted(true);
 	}
 
-	void TeleopPeriodic() {
-//		drive.DriveCartesian(s.GetRawAxis(5), s.GetRawAxis(4), s.GetRawAxis(1));
+	void TeleopPeriodic() override {
+		drive.DriveCartesian(s.GetRawAxis(5), -s.GetRawAxis(4), -s.GetRawAxis(0));
 
-//		shoot.Set((a.Get()) ? 1 : 0);
-//		aug.Set(())
+		shoot.Set(s.GetRawAxis(2));
+		intake.Set(s.GetRawAxis(3));
+
+		climber.Set((a.Get()) ? 1 : 0);
+		aug.Set((lb.Get()) ? .35 : (rb.Get()) ? -.35 : 0);
 	}
 
 private:
 
-	std::map<std::string, int> m = { { "fr", 3 }, { "fl", 4 }, { "br", 2 },
-			{ "bl", 1 }, {"shoot", 9}, {"aug", 10}};
+	std::map<std::string, int> m = { { "fr", 3 }, { "fl", 4 }, { "br", 2 }, {
+			"bl", 1 }, { "intake", 6 }, { "climber", 7 },
+			{ "climberFollow", 8 }, { "shoot", 9 }, { "aug", 10 } };
 
-	WPI_TalonSRX fr{m["fr"]};
-	WPI_TalonSRX fl{m["fl"]};
-	WPI_TalonSRX br{m["br"]};
-	WPI_TalonSRX bl{m["bl"]};
+	WPI_TalonSRX fr { m["fr"] };
+	WPI_TalonSRX fl { m["fl"] };
+	WPI_TalonSRX br { m["br"] };
+	WPI_TalonSRX bl { m["bl"] };
 
-	MecanumDrive drive{fr, fl, br, bl};
+	MecanumDrive drive { fr, fl, br, bl };
 
-	WPI_TalonSRX shoot{m["shoot"]};
-	WPI_TalonSRX aug{m["aug"]};
+	WPI_TalonSRX shoot { m["shoot"] };
+	WPI_TalonSRX aug { m["aug"] };
+	WPI_TalonSRX intake { m["intake"] };
+	WPI_TalonSRX climber { m["climber"] };
+	WPI_TalonSRX climberFollow { m["climberFollow"] };
 
-	Joystick s{0};
+	Joystick s { 0 };
 
-	JoystickButton a{&s, 1};
-	JoystickButton b{&s, 2};
-	JoystickButton x{&s, 3};
-	JoystickButton y{&s, 4};
-
-
+	JoystickButton a { &s, 1 };
+	JoystickButton lb { &s, 5 };
+	JoystickButton rb { &s, 6 };
+//	JoystickButton b{&s, 2};
+//	JoystickButton x{&s, 3};
+//	JoystickButton y{&s, 4};
 };
 
 START_ROBOT_CLASS(Robot)
